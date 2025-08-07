4395e2848aa5aab6554032ad2907f089e74f592f
subhamkumar21303 <subhamk9024@gmail.com>
2025-08-08 00:41:35 +0530

    api cleaning

--- src/app/api/bonds/owned/route.js
+++ src/app/api/bonds/owned/route.js
@@ -1,22 +1,24 @@
 export async function GET(req) {
-    try {
-      const gettingBondsURL = `${process.env.BACKEND_URL}/api/bonds/current-prices`;
-        console.log("getting bonds", gettingBondsURL);
-      const res = await fetch(gettingBondsURL, {
-        headers: {
-            'User-Agent': 'Mozilla/5.0',
-        },
-        method: 'GET',
-      });
-  
-      const data = await res.json();
-      return new Response(JSON.stringify(data), {
-        status: 200,
-        headers: { "Content-Type": "application/json" }
-      });
-    } catch (err) {
-        console.error("Error adding investment:", err);
-      return new Response(JSON.stringify({ error: err.message }), { status: 500 });
-    }
+  try {
+    const bondsURL = `${process.env.BACKEND_URL}/api/bonds/current-prices`;
+    console.log("Fetching current bond prices from:", bondsURL);
+
+    const response = await fetch(bondsURL, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0',
+      },
+    });
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error("Error fetching bond prices:", error);
+    return new Response(JSON.stringify({ error: error.message || "Internal Server Error" }), {
+      status: 500,
+    });
   }
-  
+}
